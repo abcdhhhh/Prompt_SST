a2d8d91d59209a36d00b280c4b975a53e7626463
abcdhhhh <1259232949@qq.com>
2022-01-14 16:59:02 +0800

    add f1

--- loop.py
+++ loop.py
@@ -25,17 +25,28 @@ def train_loop(model, train_dl, loss_fn, optimizer, scheduler, device):
 def dev_loop(model, dev_dl, device):
     model.eval()
     with tqdm(iterable=dev_dl) as pbar:
-        total_true = 0
-        total_len = 0
+        true_pos = 0
+        true_neg = 0
+        false_pos = 0
+        false_neg = 0
         for batch in pbar:
             pred = model(input_ids=batch[0].to(device), attention_mask=batch[1].to(device)).argmax(dim=-1)
             y = batch[2].to(device)
             # print(pred, y)
-            total_true += torch.sum(pred == y).item()
-            total_len += len(y)
-            pbar.set_postfix({"true": total_true, "len": total_len})
-        print("total_acc: ", total_true / total_len)
-        return total_true / total_len
+            true_pos += torch.sum(pred & y).item()
+            false_neg += torch.sum((pred == 0) & y).item()
+            false_pos += torch.sum(pred & (y == 0)).item()
+            true_neg += torch.sum((pred == 0) & (y == 0)).item()
+            pbar.set_postfix({"(tp,fp,fn,tn)": (true_pos, false_pos, false_neg, true_neg)})
+        true = true_pos + true_neg
+        total = true + false_pos + false_neg
+        acc = true / total
+        eps = 1e-6
+        prec = true_pos / (true_pos + false_pos + eps)
+        rec = true_pos / (true_pos + false_neg + eps)
+        f1 = 2 / (1 / (prec + eps) + 1 / (rec + eps))
+        print("acc: ", acc, "prec: ", prec, "rec: ", rec, "f1: ", f1)
+        return acc
 
 
 def test_loop(model, dev_dl, fn, device):
